4f3ac557ef325f8f3b5df6b220cb711ced726a31
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-05-13 19:46:39 +0900

    poster img 불러오기

--- lab7/js/movie-list.js
+++ lab7/js/movie-list.js
@@ -30,15 +30,15 @@ function display(movies) {
     } else {
       var actors = " ";
     }
-    // var local = movie.querySelector(".data_area > img").src;
+    var local = movie.querySelector("img").src;
     // var poster = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=상영영화" + local.substr(local.indexOf("/data"));
     string += `<table><tr><th>영화</th><td>${title}</td></tr>
                 <tr><th>장르</th><td>${genre}</td></tr>
                 <tr><th>상영시간</th><td>${runtime}</td></tr>
                 <tr><th>개봉일</th><td>${date}</td></tr>
                 <tr><th>평점</th><td>${rate}</td></tr>
-                <tr><th>출연</th><td>${actors}</td></tr>`;
-                // <tr><th>포스터</th><td><img width="178" height="260" src="${poster}"></td></tr></table>`;
+                <tr><th>출연</th><td>${actors}</td></tr>;
+                <tr><th>포스터</th><td><img width="178" height="260" src="${local}"></td></tr></table>`;
                 // 인자값 변경하기
   });            
   result.innerHTML = string;
